b5ce0bde192e3dfe3bd313a5c42873082832f8ab
mtalhaaygen <mtalha.aygen18@gmail.com>
2024-02-23 11:09:28 +0300

    main

---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,28 @@
+#include "Bureaucrat.hpp"
+#include "stdio.h"
+int main()
+{
+    Bureaucrat a;
+    Bureaucrat b(a);
+    Bureaucrat c;
+    c = a;
+
+    try
+    {
+        Bureaucrat d(-1, "d");
+    }
+    catch (std::exception &e)
+    {
+    	std::cout << e.what() << std::endl;
+    }
+    
+    try
+    {
+    	c.decrementGrade();
+    }
+    catch (Bureaucrat::GradeTooLowException &e)
+    {
+    	std::cout << e.what() << std::endl;
+    }
+    return 0;
+}
