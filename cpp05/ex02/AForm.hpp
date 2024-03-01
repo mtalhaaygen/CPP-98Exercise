e4bd9389a47a56bb091cf0ca40b9b4711675a93c
mtalhaaygen <mtalha.aygen18@gmail.com>
2024-03-01 20:08:18 +0300

    bos

--- cpp05/ex02/AForm.hpp
+++ cpp05/ex02/AForm.hpp
@@ -1,40 +1,41 @@
-#ifndef AAFORM_HPP
-#define AAFORM_HPP
+#ifndef AFORM_HPP
+#define AFORM_HPP
 
 #include <iostream>
 #include <string>
 #include "Bureaucrat.hpp"
+
 class Bureaucrat;
 
 class AForm
 {
-    private:
-        const std::string name;
-        const int gradeToSign;
-        const int gradeToExecute;
-        bool isSigned;
-    public:
-        AForm();
+	private:
+		const std::string name;
+		const int gradeToSign;
+		const int gradeToExecute;
+		bool isSigned;
+	public:
+		AForm();
 		AForm(int gradeToExecute, int gradeToSign, bool isSigned, std::string name);
-        AForm(const AForm &a);
-        AForm &operator=(const AForm &a);
-        ~AForm();
-        const std::string getName(void)const;
-        int getGradeToSign(void)const;
-        int getGradeToExecute(void)const;
-        bool getIsSigned(void)const;
-        void beSigned(Bureaucrat &a);
+		AForm(const AForm &a);
+		AForm &operator=(const AForm &a);
+		~AForm();
+		const std::string getName(void)const;
+		int getGradeToSign(void)const;
+		int getGradeToExecute(void)const;
+		bool getIsSigned(void)const;
+		void beSigned(Bureaucrat &a);
 		virtual void execute(Bureaucrat const& executor) = 0;
-        class GradeTooHighException : public std::exception
-        {
-            public:
-                const char *what() const throw();
-        };
-        class GradeTooLowException : public std::exception
-        {
-            public:
-                const char *what() const throw();
-        };
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
 };
 std::ostream & operator<<(std::ostream &o, AForm const &obj);
 
